ab5962a91529b069e5179652cc48ad2f36419222
Alexander Skrabl <alexander@skrabl.de>
2016-07-07 23:03:17 +0100

    get rid of junk and added environment file

---
+++ _ss_environment.sample.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file has to be duplicated and renamed to _ss_enviroment.php
+ */
+
+/* Environment type */
+define('SS_ENVIRONMENT_TYPE', 'dev');
+
+/* Database connection */
+define('SS_DATABASE_SERVER', 'localhost');
+define('SS_DATABASE_DATABASE', '');
+define('SS_DATABASE_USERNAME', 'root');
+define('SS_DATABASE_PASSWORD', '');
+
+/* Default username and password for admin */
+define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
+define('SS_DEFAULT_ADMIN_PASSWORD', 'password');
